713bd505e80eff1919c85ac58af09088eb8fd1ef
Ross Lancaster <toastymallows@gmail.com>
2022-02-06 13:15:26 -0500

    Update AssemblyInfo

--- src/ntoc/Properties/AssemblyInfo.cs
+++ src/ntoc/Properties/AssemblyInfo.cs
@@ -1,8 +1,11 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("ntoc")]
+[assembly: AssemblyTitle("NexoToCoinTracker")]
 [assembly: AssemblyDescription("Convert Nexo CSV files to CoinTracker CSV files")]
-[assembly: AssemblyProduct("ntoc")]
+[assembly: AssemblyProduct("NexoToCoinTracker")]
 
 [assembly: InternalsVisibleTo("ntocTests")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
